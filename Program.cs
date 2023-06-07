1dd3f38bcffe875f24e9b8ada55e07f165c9c290
Dzaki Fauzaan <cst_dev149@unitedtractors.com>
2023-06-07 22:36:24 +0700

    🐞 fix: program not working

--- Program.cs
+++ Program.cs
@@ -1,46 +1,73 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.DevTools.V107.Debugger;
-using WebDriverManager;
-using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium.Support.UI;
 
-
-var cred = File.ReadAllLines("cred.txt").ToList();
-
-System.Console.WriteLine("Sit tight, let me do all the work....");
-
-
-new DriverManager().SetUpDriver(new ChromeConfig());
 var driver = new ChromeDriver();
+try
+{
+    // Read username and password from txt file
+    string[] lines = File.ReadAllLines("cred.txt");
+    string username = lines[0];
+    string password = lines[1];
 
+    // Choose browser
 
-driver.Navigate().GoToUrl("https://heart.globalservice.co.id/");
-driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+    // Step 1: Login
+    driver.Navigate().GoToUrl("https://heart.globalservice.co.id");
+    driver.FindElement(By.Name("username")).SendKeys(username);
+    driver.FindElement(By.Name("password")).SendKeys(password);
+    driver.FindElement(By.TagName("button")).Click();
 
-var username = driver.FindElement(By.Name("username"));
-var password = driver.FindElement(By.Name("password"));
-var loginSubmitBtn = driver.FindElement(By.TagName("button"));
+    // Wait for dashboard page to load
+    WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+    wait.Until(d => d.Url.Contains("dashboard"));
 
-username.SendKeys(cred[0]);
-password.SendKeys(cred[1]);
-loginSubmitBtn.Click();
+    // Redirect to Absensi page
+    driver.Navigate().GoToUrl("https://heart.globalservice.co.id/staff/absensi");
 
-driver.Navigate().GoToUrl("https://heart.globalservice.co.id/staff/absensi");
+    // Wait for Absensi page to load
+    wait.Until(d => d.Url.Contains("absensi"));
 
-var mood = driver.FindElement(By.CssSelector("[src*='https://heart.globalservice.co.id/assets/img/emoji/3.png']"));
-var desc = driver.FindElement(By.Id("note"));
-var submitBtn = driver.FindElement(By.CssSelector("[type*='submit']"));
+    try
+    {
+        // Allow location permission
+        IAlert alert = driver.SwitchTo().Alert();
+        alert.Accept();
+    }
+    catch (NoAlertPresentException)
+    {
 
-desc.SendKeys($"{submitBtn.Text} at {Round(DateTime.Now, TimeSpan.FromMinutes(1)).ToString("H:mm")}");
-mood.Click();
+    }
 
-submitBtn.Click();
+    // Step 2: Perform check-in
+    var btnSubmit = driver.FindElement(By.CssSelector("[type*='submit']"));
+    IWebElement noteElement = driver.FindElement(By.Id("note"));
+    noteElement.Clear();
+    noteElement.SendKeys($"{btnSubmit.Text} at {Round(DateTime.Now, TimeSpan.FromMinutes(1)).ToString("H:mm")}");
+    Thread.Sleep(1000);
 
-Thread.Sleep(3000);
-driver.Quit();
-return;
+    IWebElement emojiElement = driver.FindElement(By.CssSelector("img[src='https://heart.globalservice.co.id/assets/img/emoji/3.png']"));
+    emojiElement.Click();
 
-static DateTime Round(DateTime date, TimeSpan interval) {
+    Thread.Sleep(10000);
+    btnSubmit.Submit();
+    WebDriverWait modalWait = new WebDriverWait(driver, TimeSpan.FromSeconds(100));
+    modalWait.Until(d => d.FindElement(By.ClassName("swal-modal")).Displayed);
+
+    // Close the modal by clicking the OK button
+    IWebElement okButton = driver.FindElement(By.CssSelector(".swal-button--confirm"));
+    okButton.Click();
+
+    // Close the browser
+    driver.Quit();
+}
+catch (System.Exception)
+{
+    driver.Quit();
+}
+
+DateTime Round(DateTime date, TimeSpan interval)
+{
     return new DateTime(
         (long)Math.Round(date.Ticks / (double)interval.Ticks) * interval.Ticks
     );
